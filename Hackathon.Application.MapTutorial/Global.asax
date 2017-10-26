1761ee6f1b912702d27fb4938b9d08b87e41bdd9
restav <mhebalci@gmail.com>
2017-10-26 14:56:44 +0300

    First Commit

---
+++ Hackathon.Application.MapTutorial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hackathon.Application.MapTutorial.MvcApplication" Language="C#" %>
---
+++ Hackathon.Common.Network/HttpHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Common.Network
+{
+    public class HttpHelper
+    {
+    }
+}
